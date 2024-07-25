11a1738d2f050024603517da990b74d9d9c01245
rejaul karim <devrejaul.official@gmail.com>
2024-07-25 16:40:16 +0600

    add favorite route

--- src/favorite/controller.ts
+++ src/favorite/controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
 import { Service } from './service';
 
 @Controller('favorite')
@@ -13,5 +13,10 @@ export class FavoriteController {
   @Get(':id')
   getFavorite(@Param() {id}) {
     return this.service.getFavorite(id);
+  }
+  
+  @Delete(':id')
+  delete(@Param() {id}) {
+    return this.service.delete(id);
   } 
 }
--- src/favorite/service.ts
+++ src/favorite/service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Favorite } from './model';
+import { Music } from 'src/music/model';
 
 @Injectable()
 export class Service {
@@ -9,8 +10,6 @@ export class Service {
     private readonly model: typeof Favorite,
   ) { }
     
-
-    
   async create(body) { 
     const favorite = new Favorite(body);
     return await favorite.save();
@@ -18,10 +17,18 @@ export class Service {
 
 
   async getFavorite(id: string) {
-   
-    const favorite = await this.model.findOne({ where: { id: id } });
-    console.log(favorite);
-    
+    const favorite = await this.model.findAll({ where: { userId: id } });
+    const favoriteMusics = [];
+    if (favorite.length <= 0) return favorite;
+    // is not good practice, just for testing //  
+    for (let i = 0; i < favorite.length; i++) {
+      const music = await Music.findOne({ where: { id: favorite[i].musicId } });
+      favoriteMusics.push(music);
+    }
+    return favoriteMusics;
   }
 
+  async delete(id: string) {
+    return await this.model.destroy({ where: {id} });
+  }
 }
